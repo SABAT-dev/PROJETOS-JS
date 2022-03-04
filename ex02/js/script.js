98f749c6eb991729d82454dbfba3cee6668ca1b8
Your Name <you@example.com>
2022-03-04 15:24:05 -0300

    Aula 06 - Tratamento de Dados

---
+++ ex02/js/script.js
@@ -0,0 +1,5 @@
+var n1 = Number(window.prompt("Digite o primeiro número:"));
+var n2 = Number(window.prompt("Digite o segundo número:"));
+var s = n1 + n2;
+
+window.alert(`O resultado da soma entre ${n1} + ${n2} = ` + s.toString());
